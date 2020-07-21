89b4fdf73a2c823c13ec4bf9865b0f958afe5a6f
Lautaro Bifano <bifanolautaro@gmail.com>
2020-07-20 22:41:58 -0300

    CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
